1d1db2ac226b84809b2653ddb5cb115bf4a23c07
Xabier Larrakoetxea <slok69@gmail.com>
2018-01-31 20:25:57 +0100

    Implement the ability to wait unit the crd is ready
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- client/crd/crd.go
+++ client/crd/crd.go
@@ -10,6 +10,12 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/spotahome/kooper/log"
+	wraptime "github.com/spotahome/kooper/wrapper/time"
+)
+
+const (
+	checkCRDInterval = 500 * time.Millisecond
+	crdReadyTimeout  = 30 * time.Second
 )
 
 // Scope is the scope of a CRD.
@@ -31,34 +37,45 @@ type Conf struct {
 	Scope      Scope
 }
 
+func (c *Conf) getName() string {
+	return fmt.Sprintf("%s.%s", c.NamePlural, c.Group)
+}
+
 // Interface is the CRD client that knows how to interact with k8s to manage them.
 type Interface interface {
 	// EnsureCreated will ensure the the CRD is present.
 	EnsurePresent(conf Conf) error
-	// WaitToBePresent will wait until the CRD is present.
-	WaitToBePresent(conf Conf, timeout time.Duration) error
+	// WaitToBePresent will wait until the CRD is present or it timesout.
+	WaitToBePresent(name string, timeout time.Duration) error
 	// Delete will delete the CRD.
-	Delete(conf Conf) error
+	Delete(name string) error
 }
 
 // Client is the CRD client implementation using API calls to kubernetes.
 type Client struct {
 	aeClient apiextensionscli.Interface
 	logger   log.Logger
+	time     wraptime.Time // Use a time wrapper so we can control the time on our tests.
 }
 
 // NewClient returns a new CRD client.
 func NewClient(aeClient apiextensionscli.Interface, logger log.Logger) *Client {
+	return NewCustomClient(aeClient, wraptime.Base, logger)
+}
+
+// NewCustomClient returns a new CRD client letting you set all the required parameters
+func NewCustomClient(aeClient apiextensionscli.Interface, time wraptime.Time, logger log.Logger) *Client {
 	return &Client{
 		aeClient: aeClient,
 		logger:   logger,
+		time:     time,
 	}
 }
 
 // EnsurePresent satisfies crd.Interface.
 func (c *Client) EnsurePresent(conf Conf) error {
 	// TODO: Check version of cluster equal or greater than 1.7
-	crdName := fmt.Sprintf("%s.%s", conf.NamePlural, conf.Group)
+	crdName := conf.getName()
 
 	crd := &apiextensionsv1beta1.CustomResourceDefinition{
 		ObjectMeta: metav1.ObjectMeta{
@@ -82,17 +99,34 @@ func (c *Client) EnsurePresent(conf Conf) error {
 		}
 		return nil
 	}
-	c.logger.Infof("crd %s created", crdName)
-	// TODO: wait to be present.
+
+	c.logger.Infof("crd %s created, waitint to be ready...", crdName)
+	c.WaitToBePresent(crdName, crdReadyTimeout)
+	c.logger.Infof("crd %s ready", crdName)
+
 	return nil
 }
 
 // WaitToBePresent satisfies crd.Interface.
-func (c *Client) WaitToBePresent(conf Conf, timeout time.Duration) error {
-	return fmt.Errorf("Not implemented")
+func (c *Client) WaitToBePresent(name string, timeout time.Duration) error {
+	tout := c.time.After(timeout)
+	t := c.time.NewTicker(checkCRDInterval)
+
+	for {
+		select {
+		case <-t.C:
+			_, err := c.aeClient.ApiextensionsV1beta1().CustomResourceDefinitions().Get(name, metav1.GetOptions{})
+			// Is present, finish.
+			if err == nil {
+				return nil
+			}
+		case <-tout:
+			return fmt.Errorf("timeout waiting for CRD")
+		}
+	}
 }
 
 // Delete satisfies crd.Interface.
-func (c *Client) Delete(conf Conf) error {
-	return fmt.Errorf("Not implemented")
+func (c *Client) Delete(name string) error {
+	return c.aeClient.ApiextensionsV1beta1().CustomResourceDefinitions().Delete(name, &metav1.DeleteOptions{})
 }
--- client/crd/crd_test.go
+++ client/crd/crd_test.go
@@ -2,9 +2,12 @@ package crd_test
 
 import (
 	"errors"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	apiextensionscli "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/fake"
 	kubeerrors "k8s.io/apimachinery/pkg/api/errors"
@@ -15,18 +18,31 @@ import (
 
 	"github.com/spotahome/kooper/client/crd"
 	"github.com/spotahome/kooper/log"
+	mtime "github.com/spotahome/kooper/mocks/wrapper/time"
 )
+
+var (
+	crdGroup = schema.GroupVersionResource{Group: "apiextensions.k8s.io", Version: "v1beta1", Resource: "customresourcedefinitions"}
+)
+
+func newCRDGetAction(name string) kubetesting.GetActionImpl {
+	return kubetesting.NewGetAction(crdGroup, "", name)
+}
+
+func newCRDCreateAction(crd *apiextensionsv1beta1.CustomResourceDefinition) kubetesting.CreateActionImpl {
+	return kubetesting.NewCreateAction(crdGroup, "", crd)
+}
 
 func TestCRDEnsurePresent(t *testing.T) {
 	tests := []struct {
-		name   string
-		crd    crd.Conf
-		expCrd *apiextensionsv1beta1.CustomResourceDefinition
-		retErr error
-		expErr bool
+		name     string
+		crd      crd.Conf
+		retErr   error
+		expErr   bool
+		expCalls []kubetesting.Action
 	}{
 		{
-			name: "Creating a non existe CRD should create a crd without error",
+			name: "Creating a non existen CRD should create a crd without error",
 			crd: crd.Conf{
 				Kind:       "Test",
 				NamePlural: "tests",
@@ -34,22 +50,25 @@ func TestCRDEnsurePresent(t *testing.T) {
 				Group:      "toilettesting",
 				Version:    "v99",
 			},
-			expCrd: &apiextensionsv1beta1.CustomResourceDefinition{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "tests.toilettesting",
-				},
-				Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
-					Group:   "toilettesting",
-					Version: "v99",
-					Scope:   crd.ClusterScoped,
-					Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
-						Plural: "tests",
-						Kind:   "Test",
-					},
-				},
-			},
 			retErr: nil,
 			expErr: false,
+			expCalls: []kubetesting.Action{
+				newCRDCreateAction(&apiextensionsv1beta1.CustomResourceDefinition{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "tests.toilettesting",
+					},
+					Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
+						Group:   "toilettesting",
+						Version: "v99",
+						Scope:   crd.ClusterScoped,
+						Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
+							Plural: "tests",
+							Kind:   "Test",
+						},
+					},
+				}),
+				newCRDGetAction("tests.toilettesting"),
+			},
 		},
 		{
 			name: "Creating a CRD that errors should return an error.",
@@ -60,9 +79,9 @@ func TestCRDEnsurePresent(t *testing.T) {
 				Group:      "toilettesting",
 				Version:    "v99",
 			},
-			expCrd: nil,
-			retErr: errors.New("wanted error"),
-			expErr: true,
+			retErr:   errors.New("wanted error"),
+			expErr:   true,
+			expCalls: []kubetesting.Action{},
 		},
 		{
 			name: "Creating a CRD that exists shouldn't return an error.",
@@ -73,19 +92,21 @@ func TestCRDEnsurePresent(t *testing.T) {
 				Group:      "toilettesting",
 				Version:    "v99",
 			},
-			expCrd: &apiextensionsv1beta1.CustomResourceDefinition{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "tests.toilettesting",
-				},
-				Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
-					Group:   "toilettesting",
-					Version: "v99",
-					Scope:   crd.ClusterScoped,
-					Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
-						Plural: "tests",
-						Kind:   "Test",
+			expCalls: []kubetesting.Action{
+				newCRDCreateAction(&apiextensionsv1beta1.CustomResourceDefinition{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "tests.toilettesting",
 					},
-				},
+					Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
+						Group:   "toilettesting",
+						Version: "v99",
+						Scope:   crd.ClusterScoped,
+						Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
+							Plural: "tests",
+							Kind:   "Test",
+						},
+					},
+				}),
 			},
 			retErr: kubeerrors.NewAlreadyExists(schema.GroupResource{}, ""),
 			expErr: false,
@@ -101,22 +122,77 @@ func TestCRDEnsurePresent(t *testing.T) {
 			cli.AddReactor("create", "customresourcedefinitions", func(action kubetesting.Action) (bool, runtime.Object, error) {
 				return true, nil, test.retErr
 			})
+			cli.AddReactor("get", "customresourcedefinitions", func(action kubetesting.Action) (bool, runtime.Object, error) {
+				return true, nil, nil
+			})
 
 			crdCli := crd.NewClient(cli, log.Dummy)
 			err := crdCli.EnsurePresent(test.crd)
 			if test.expErr {
 				assert.Error(err)
 			} else if assert.NoError(err) {
-				// Check the client calls.
-				actions := cli.Actions()
-				if assert.Len(actions, 1) {
-					createAction, ok := actions[0].(kubetesting.CreateActionImpl)
-					if assert.True(ok, "the action should be a creation") {
-						assert.Equal(test.expCrd, createAction.GetObject())
-					}
-				}
+				assert.Equal(test.expCalls, cli.Actions())
 			}
 
+		})
+	}
+}
+
+func TestCRDWaitToBePresent(t *testing.T) {
+	never := 999999 * time.Hour
+	tests := []struct {
+		name       string
+		crdName    string
+		timeout    time.Duration
+		interval   time.Duration
+		readyAfter time.Duration
+		expErr     bool
+	}{
+		{
+			name:       "If timeouts it should return an error",
+			crdName:    "test",
+			timeout:    1,
+			interval:   never,
+			readyAfter: never,
+			expErr:     true,
+		},
+		{
+			name:       "After some time the CRD will be ready and won't timeout",
+			crdName:    "test",
+			timeout:    50 * time.Millisecond,
+			interval:   5 * time.Millisecond,
+			readyAfter: 25 * time.Millisecond,
+			expErr:     false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			// Mock calls
+			mt := &mtime.Time{}
+			mt.On("After", mock.Anything).Once().Return(time.After(test.timeout))
+			mt.On("NewTicker", mock.Anything).Once().Return(time.NewTicker(test.interval))
+			cli := &apiextensionscli.Clientset{}
+			start := time.Now()
+			cli.AddReactor("get", "customresourcedefinitions", func(action kubetesting.Action) (bool, runtime.Object, error) {
+				// Get how long we've been running and if it passed readyAfter
+				// our CRD is ready.
+				runningTime := time.Now().Sub(start)
+				if runningTime >= test.readyAfter {
+					return true, nil, nil
+				}
+				return true, nil, fmt.Errorf("wanted error")
+
+			})
+
+			crdCli := crd.NewCustomClient(cli, mt, log.Dummy)
+			err := crdCli.WaitToBePresent(test.crdName, 0)
+			if test.expErr {
+				assert.Error(err)
+			} else {
+				assert.NoError(err)
+			}
 		})
 	}
 }
