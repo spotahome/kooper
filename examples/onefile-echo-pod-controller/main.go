9166f4ab17a4d6d654edba6c92b66750d1e8f593
Xabier Larrakoetxea <slok69@gmail.com>
2018-02-13 13:10:03 +0100

    Add to echo-pod-controller the oidc auth
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- examples/onefile-echo-pod-controller/main.go
+++ examples/onefile-echo-pod-controller/main.go
@@ -10,6 +10,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
