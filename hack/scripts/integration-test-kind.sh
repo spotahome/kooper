029477e352873654bd0609a2719701e0536440ea
Sergio Ballesteros <snaker@locolandia.net>
2019-11-25 22:17:24 +0100

    Fix kind for integration tests

--- hack/scripts/integration-test-kind.sh
+++ hack/scripts/integration-test-kind.sh
@@ -5,28 +5,22 @@ set -o nounset
 
 KUBERNETES_VERSION=v${KUBERNETES_VERSION:-1.14.9}
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-SUDO=''
-if [[ $(id -u) -ne 0 ]]
-then
-    SUDO="sudo"
-fi
+PREVIOUS_KUBECTL_CONTEXT=$(kubectl config current-context) || PREVIOUS_KUBECTL_CONTEXT=""
 
 function cleanup {
+    if [ ! -z $PREVIOUS_KUBECTL_CONTEXT ]
+    then
+      kubectl config use-context $PREVIOUS_KUBECTL_CONTEXT
+    fi
     echo "=> Removing kind cluster"
-    $SUDO kind delete cluster
+    kind delete cluster
 }
 trap cleanup EXIT
 
-echo "=> Preparing minikube for running integration tests"
-$SUDO kind create cluster --image kindest/node:${KUBERNETES_VERSION}
+echo "=> Preparing kind for running integration tests"
+kind create cluster --image kindest/node:${KUBERNETES_VERSION}
 
-echo "=> Waiting to start cluster..."
-sleep 30
-
-export KUBECONFIG="$(kind get kubeconfig-path)"
-
-$SUDO chmod a+r ${KUBECONFIG}
+kubectl config use-context kind-kind
 
 echo "=> Running integration tests"
 ${current_dir}/integration-test.sh
