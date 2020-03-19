00f8861d1aee3d7e55dd1cd584ba4896b51bc1cb
Xabier Larrakoetxea <me@slok.dev>
2020-03-19 11:38:01 +0100

    Decouple controller handler metrics into a decorator
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

--- controller/handler.go
+++ controller/handler.go
@@ -3,8 +3,11 @@ package controller
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/spotahome/kooper/monitoring/metrics"
 )
 
 // Handler knows how to handle the received resources from a kubernetes cluster.
@@ -40,4 +43,46 @@ func (h *HandlerFunc) Delete(ctx context.Context, s string) error {
 		return fmt.Errorf("function can't be nil")
 	}
 	return h.DeleteFunc(ctx, s)
+}
+
+type metricsMeasuredHandler struct {
+	id   string
+	mrec metrics.Recorder
+	next Handler
+}
+
+func newMetricsMeasuredHandler(id string, mrec metrics.Recorder, next Handler) Handler {
+	return metricsMeasuredHandler{
+		id:   id,
+		mrec: mrec,
+		next: next,
+	}
+}
+
+func (m metricsMeasuredHandler) Add(ctx context.Context, obj runtime.Object) (err error) {
+	defer func(start time.Time) {
+		m.mrec.ObserveDurationResourceEventProcessed(m.id, metrics.AddEvent, start)
+
+		if err != nil {
+			m.mrec.IncResourceEventProcessedError(m.id, metrics.AddEvent)
+		}
+	}(time.Now())
+
+	m.mrec.IncResourceEventProcessed(m.id, metrics.AddEvent)
+
+	return m.next.Add(ctx, obj)
+}
+
+func (m metricsMeasuredHandler) Delete(ctx context.Context, objKey string) (err error) {
+	defer func(start time.Time) {
+		m.mrec.ObserveDurationResourceEventProcessed(m.id, metrics.DeleteEvent, start)
+
+		if err != nil {
+			m.mrec.IncResourceEventProcessedError(m.id, metrics.DeleteEvent)
+		}
+	}(time.Now())
+
+	m.mrec.IncResourceEventProcessed(m.id, metrics.DeleteEvent)
+
+	return m.next.Delete(ctx, objKey)
 }
